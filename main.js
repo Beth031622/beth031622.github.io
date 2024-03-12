ae7e99547611dd3e9f121e03fd85405761d5c04c
Beth031622 <163068554+Beth031622@users.noreply.github.com>
2024-03-11 21:00:31 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function acordeon(id) {
+    let div = document.getElementById(id); 
+    if (div.style.display == "flex"){ 
+      div.style.display = "none";
+    } 
+    else {
+      div.style.display = "flex";
+    }
+}
+
